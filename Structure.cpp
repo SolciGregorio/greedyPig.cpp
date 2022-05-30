2aea9c387a7119cf55317b5be654c0851913a615
Solci Gregorio <83075808+SolciGregorio@users.noreply.github.com>
2022-05-30 19:18:55 -0300

    Create Structure.cpp

---
+++ Structure.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    int x;
+    //OPCIONES CON UN SWITCH
+    //QUIEN ARRANCA PRIMERO
+ 
+ 
+    for(x=0;x<5;x++){
+ 
+        //JUGADOR 1
+        //doWhile preguntar si tira de nuevo CON UN BOOL
+        do{
+ 
+        }(while);
+ 
+ 
+        //JUGADOR 2
+        //doWhile preguntar si tira de nuevo CON UN BOOL
+        do{
+ 
+        }(while);
+ 
+ 
+ 
+    }
+ 
+ 
+ 
+ 
+ 
+    return 0;
+}
